141c6e002363c5e129ebe8e5ef7f37ec5af80543
Akash140605 <akashcomputerbisalpur@gmail.com>
2024-12-25 19:28:08 +0530

    Create Experience.js

---
+++ src/components/Experience.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <section id="experience" className="p-5 bg-light">
+      <h2>Professional Experience</h2>
+      <ul>
+        <li>
+          <strong>Summer Training at Uniconverge Technologies, Noida</strong>{" "}
+          (June 2024 - August 2024): Gained hands-on experience in IoT,
+          embedded systems, and software development.
+        </li>
+        <li>
+          <strong>Founding Member of IoT Club, M.J.P.R.U</strong>: Led
+          initiatives and projects in IoT and related technologies.
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Experience;
