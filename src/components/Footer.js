a029145df934f1cfba57c2cf46e3ed135b277ad3
Akash140605 <akashcomputerbisalpur@gmail.com>
2024-12-25 19:28:39 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center p-3 bg-dark text-white">
+      <p>Contact: (+91) 7351363836 | lalitgangwar04@gmail.com</p>
+      <a href="https://shorturl.at/L40qo" target="_blank" rel="noopener noreferrer">
+        Portfolio Link
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
