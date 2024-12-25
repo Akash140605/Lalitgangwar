d233268ca12b79b0f12c38bdcb31c40f86a68593
Akash140605 <akashcomputerbisalpur@gmail.com>
2024-12-26 00:34:05 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,7 +40,7 @@ const Projects = () => {
     <section id="projects" className="projects-section bg-transparent container py-5">
       <header className="text-center mb-5">
         <motion.h1 
-          className="display-4 fw-bold"
+          className="display-4 fw-bold text-light"
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
